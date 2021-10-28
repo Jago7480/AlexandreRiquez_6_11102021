19acde4cd9d80c2aac990641dc3a7444dc98549c
AlexandreR <alexandre.riquez@gmail.com>
2021-10-28 09:09:46 +0200

    correction du "passer au contenu" et ajout de labels aria

--- js/index.js
+++ js/index.js
@@ -1,13 +1,22 @@
+//PASSER AU CONTENU
+
 //Création de la function onscroll
 window.onscroll = function() {myFunction()};
 
 //Si la page est scroll à "50" pixels, l'élément sélectionné apparait. Il disparait quand nous revenons en haut de la page.
 function myFunction() {
     if (document.scrollTop > 50 || document.querySelector.scrollTop > 50) {
-        // const btnContenu = document.querySelector('#retour-contenu');
-        // btnContenu.style.display = "none"
+        const btnContenu = document.querySelector('#retour-contenu');
+        btnContenu.style.display = "none"
     } else {
         const btnContenu = document.querySelector('#retour-contenu');
         btnContenu.style.display = "flex";
     }
 }
+
+// TRI PAR TAG
+
+
+
+// AFFICHAGE DYNAMIQUE PHOTOGRAPHES
+
